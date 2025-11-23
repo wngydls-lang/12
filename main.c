372851da2b9c616f0dfae273b7ee4f047646b06f
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-11-23 22:13:56 +0900

    12-1

---
+++ main.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+srand((unsigned) (time(NULL)));
+
+//opening
+printf("~~start~~~\n");
+
+//step.1](//step.1) initialization (player name setting)
+//step.2](//step.2) turn play (do-while)
+//2-1. status printing
+//2-2. roll die
+//2-3. move (result)
+//2-4. change turn, shark move
+
+//step.3](//step.3) game end (winner printing)
+
+printf("~~~~~~end~~~\n");
+system("PAUSE");	
+return 0;
+}
